f6657ad0755e19b66244c1121d87b10ccb28c0c2
Sahil Changede <sahil.changede@samsanlabs.com>
2025-07-11 11:20:25 +0530

    Changed the frontend url

--- src/main/java/Sulekhaai/WHBRD/Websocket/WebSocketConfig.java
+++ src/main/java/Sulekhaai/WHBRD/Websocket/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                     "http://localhost:5173",
                     "http://192.168.1.63:5173",
                     "http://192.168.1.63:5174",
-                    "https://sulekha-ai.netlify.app"
+                    "https://sulekha-aii.netlify.app"
                 ); // ✅ Add Render frontend too if needed
         // ⚠️ Do not add .withSockJS() unless your frontend explicitly uses SockJS
     }
