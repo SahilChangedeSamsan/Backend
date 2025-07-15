1a1912d3d3769a2a9e88eb89128ebe998d84d9ca
Sahil Changede <sahil.changede@samsanlabs.com>
2025-07-15 16:00:11 +0530

    Modified the code of the RecordingService.java

--- src/main/java/Sulekhaai/WHBRD/repository/SessionImageRepository.java
+++ src/main/java/Sulekhaai/WHBRD/repository/SessionImageRepository.java
@@ -3,7 +3,7 @@ package Sulekhaai.WHBRD.repository;
 import Sulekhaai.WHBRD.model.SessionImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-
+ 
 public interface SessionImageRepository extends JpaRepository<SessionImage, Long> {
     List<SessionImage> findBySessionId(Long sessionId);
 } 
