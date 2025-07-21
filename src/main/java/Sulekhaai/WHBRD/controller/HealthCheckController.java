6ba7991d1986f65117c79bd763eb32722872127e
Mayurs1919 <mayur.shinde@samsanlabs.com>
2025-07-21 16:05:18 +0530

    Updated the health checker for backend

--- src/main/java/Sulekhaai/WHBRD/controller/HealthCheckController.java
+++ src/main/java/Sulekhaai/WHBRD/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.yourpackage.controller;
+package Sulekhaai.WHBRD.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
